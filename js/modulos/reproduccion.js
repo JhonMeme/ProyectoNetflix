7151d5e66de1217aad4bb9953e2b41be870835b5
JhonMeme <emerxxithoz@gmail.com>
2023-08-09 22:55:11 -0500

    p

--- js/modulos/reproduccion.js
+++ js/modulos/reproduccion.js
@@ -1,7 +1,9 @@
-window.addEventListener("activar", function() {
-    const activar = document.querySelector("#Activar");
-
-    if (checkbox === 0){
-        
-    }
-})
+window.addEventListener('DOMContentLoaded'), () => {
+    // Obtener todos los elementos de video
+    const videos = document.querySelectorAll('video');
+    
+    // Ocultar los controles de todos los elementos de video
+    videos.forEach(video => {
+       video.removeAttribute('controls');
+    });
+}
